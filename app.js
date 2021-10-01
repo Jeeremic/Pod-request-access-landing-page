5f8615ddd3bca07bf8abe5cb4440e0b5d4e51034
Stefan Jeremic <jeremicss92gmail.com>
2021-10-01 12:33:34 +0200

    fix visibility

--- app.js
+++ app.js
@@ -7,17 +7,16 @@ button.addEventListener('click', oneClick);
 var reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function oneClick(e) {
-    //debugger;
     e.preventDefault();
     if (emailInput.value === '' || !reg.test(emailInput.value)) {
         msg.classList.add('error');
         msg.innerHTML = 'Oops! Please check your email';
-        setTimeout(() => msg.remove(), 3000);
+        setTimeout(() => msg.classList.remove('error'), 3000);
     } else {
         msg2.classList.add('goodMail');
         msg2.innerHTML = 'Successful!';    
-        setTimeout(() => msg2.remove(), 3000);
+        setTimeout(() => msg2.classList.remove('goodMail'), 3000);
     }
     return;
-
 } 
+
